4cf3e63aebe26dd480e190411f3cb1dd0d2f7426
JuliaOlssonR <84335141+JuliaOlssonR@users.noreply.github.com>
2021-11-17 17:49:57 -0300

    Update Teste2.py

--- Teste2.py
+++ Teste2.py
@@ -1,11 +1,13 @@
-from math import log,factorial
-from statistics import mean
+from math import log,factorial #importa as funções ln e fatorial do módulo math
+from statistics import mean # importa a função média do módulo de estatística
 
-x = [0]*10
-for i in range(10):
-    if i%2==0:
-        x[i] = 3+7*factorial(i)
-    else:
-        x[i] = 2+4*log(i)
-print('A posição do maior elemento é', n.index(max(n)))
-print('A média dos elementos contidos nesse vetor é',round(mean(n),2))
+x = [0]*10 # cria um vetor com 10 elementos
+for i in range(10): # para cada número inteiro entre 0 e 10
+    if i%2==0: # caso o número i seja par
+        x[i] = 3+7*factorial(i) # o valor do elemento i no vetor x é substituida pela fórmula indicada
+    else: #  caso o numero i seja ímpar
+        x[i] = 2+4*log(i) # o valor do elemento i no vetor x é substituida pela fórmula indicada
+indice_max = n.index(max(x)) # encontra o índice para o elemento máximo de x
+valor_medio = round(mean(x),2) # calcula o valor da média dos elementos de x arredondado para duas casas decimais 
+print('A posição do maior elemento é', indice_max) 
+print('A média dos elementos contidos nesse vetor é', valor_medio)
