04a0a88d88727e6df3d14d96e1ec93769f83ee64
JuliaOlssonR <84335141+JuliaOlssonR@users.noreply.github.com>
2021-11-17 17:54:02 -0300

    Update Teste3.py

--- Teste3.py
+++ Teste3.py
@@ -1,7 +1,8 @@
-dicio = {}
-for i in range(5):
-    nome = input('Nome do aluno:')
-    nota = int(input('Nota:'))
-    dicio[nome]= nota
-maior_nota = max(dicio,key=dicio.get)
-print('O aluno que tirou a maior nota foi',maior_nota,', com nota', dicio[max(dicio,key=dicio.get)])
+dicio = {} # cria um dicionário
+for i in range(5): # loop para fazer os inputs dos dados de 5 alunos
+    nome = input('Nome do aluno:') # input para inserir o nome do aluno
+    nota = float(input('Nota:')) # input para inserir a nota do aluno
+    dicio[nome]= nota #cria a chave com o nome do aluno no dicionário cujo valor é a nota do aluno
+maior_nota = max(dicio,key=dicio.get) # encontra a chave que tem o maior valor do dicionário
+
+print('O aluno que tirou a maior nota foi',maior_nota,', com nota', dicio[max(dicio,key=dicio.get)]) # imprime o aluno com a maior nota e a respectiva nota que ele tirou
